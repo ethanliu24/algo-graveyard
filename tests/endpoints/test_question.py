c8b4a193f13700eda8edd7c0219dc117f3bec3bd
ethanliu24 <liu.ethan2005@gmail.com>
2025-03-29 00:27:24 -0400

    add post tests for qs

--- tests/endpoints/test_question.py
+++ tests/endpoints/test_question.py
@@ -6,6 +6,7 @@ from tests.seed import QUESTIONS
 
 API = "/api/questions"
 
+# Getting Questions
 @pytest.mark.asyncio
 async def test_get_all_questions_no_filter(endpoint):
     """ Test the properties of the result of the endpoint to get all questions_no_filter """
@@ -34,3 +35,59 @@ async def test_get_question_doesnt_exist(endpoint):
     """ Test if endpoint when question id doesn't exist in the database. """
     response = endpoint.get(f"{API}/question_dne")
     assert response.status_code == 404
+
+
+# Creating questions
+@pytest.mark.asyncio
+async def test_create_question_basic(endpoint):
+    """ Test creating a question with basic inputs. """
+    question = {
+        "source": "leetcode",
+        "link": "",
+        "status": "completed",
+        "title": "Create Question Basic",
+        "prompt": "create a basic question",
+        "test_cases": [],
+        "notes": [],
+        "hints": [],
+        "tags": []
+    }
+
+    response = endpoint.post(f"{API}", json=question)
+    assert response.status_code == 200
+    assert isinstance(response.text, str)
+
+
+@pytest.mark.asyncio
+async def test_create_question_invalid_input(endpoint):
+    """ Test creating a question with invalid input values. """
+    question = {
+        "source": "l",
+        "link": "",
+        "status": "",
+        "title": "Create Question Basic",
+        "prompt": "create a basic question",
+        "test_cases": [],
+        "notes": [],
+        "hints": [],
+        "tags": []
+    }
+
+    response = endpoint.post(f"{API}", json=question)
+    assert response.status_code == 422
+
+
+@pytest.mark.asyncio
+async def test_create_question_missing_fields(endpoint):
+    """ Test creating a question with missing fields. """
+    question = {
+        "link": "",
+        "status": "",
+        "title": "Create Question Basic",
+        "test_cases": [],
+        "hints": [],
+        "tags": []
+    }
+
+    response = endpoint.post(f"{API}", json=question)
+    assert response.status_code == 422
