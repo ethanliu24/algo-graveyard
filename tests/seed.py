4b7ea5faea98f31e37a258ec2f0ba47006ac1aff
ethanliu24 <liu.ethan2005@gmail.com>
2025-04-01 08:46:02 -0400

    add to test seed

--- tests/seed.py
+++ tests/seed.py
@@ -1,6 +1,41 @@
 from app.schemas.question import Source, Status
 from datetime import datetime, timezone
 
+SOLUTIONS = [
+    {
+        "id": "s1",
+        "summary": "Test",
+        "explanation": "test",
+        "language": "python",
+        "time_complexity": "n!",
+        "space_complexity": "n^n",
+        "ai_analysis": {
+            "time_complexity": "n!",
+            "space_complexity": "n^n",
+            "feedback": "trash",
+        },
+        "code": "print(\"hello world\")",
+        "created_at": datetime(2011, 1, 1, 1, 1, 1, 1, tzinfo=timezone.utc),
+        "last_modified": datetime(2011, 1, 1, 1, 1, 1, 1, tzinfo=timezone.utc),
+    },
+    {
+        "id": "s2",
+        "summary": "Test 2",
+        "explanation": "test 2",
+        "language": "python",
+        "time_complexity": "n!",
+        "space_complexity": "n^n",
+        "ai_analysis": {
+            "time_complexity": "n!",
+            "space_complexity": "n^n",
+            "feedback": "trash",
+        },
+        "code": "print(\"hello world\")",
+        "created_at": datetime(2011, 1, 1, 1, 1, 1, 1, tzinfo=timezone.utc),
+        "last_modified": datetime(2011, 1, 1, 1, 1, 1, 1, tzinfo=timezone.utc),
+    }
+]
+
 QUESTIONS = [
     {
         "id": "q1",
@@ -13,7 +48,7 @@ QUESTIONS = [
         "notes": [],
         "hints": [],
         "tags": [],
-        "solutions": [],
+        "solutions": [SOLUTIONS[0]],
         "created_at": datetime(2011, 1, 1, 1, 1, 1, 1, tzinfo=timezone.utc),
         "last_modified": datetime(2011, 1, 1, 1, 1, 1, 1, tzinfo=timezone.utc),
     },
