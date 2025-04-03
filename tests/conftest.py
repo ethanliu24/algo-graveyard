753e4e4cbdeddf2baff25e7b0b8bd924c2571932
ethanliu24 <liu.ethan2005@gmail.com>
2025-04-02 21:26:29 -0400

    move populate and clear db fns to utils

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,72 +3,14 @@ import pytest
 import utils
 
 from app.config import Configs
-from app.env_vars import ENV_VARS
 from app.main import app
-from app.schemas.ai_analysis import AiAnalysis
-from app.schemas.question import Question
-from app.schemas.solution import Solution
-from app.schemas.test_case import TestCase
-from app.schemas.token import Token
-from .seed import QUESTIONS
 
 def pytest_sessionstart():
-    """ Populates the database when a test session starts. """
-    configs = Configs()
-    client = configs.firebase_manager.get_client()
-
-    seen_q = set()
-    for q_data in QUESTIONS:
-        # sanity check for duplication
-        q_id = q_data["id"]
-        if q_id in seen_q:
-            raise ValueError("Duplicate ID. Check if database question IDs are unique.")
-        seen_q.add(q_id)
-
-        # validate solutions
-        seen_sln = set()
-        for sln_data in q_data["solutions"]:
-            # sanity check for duplication
-            sln_id = sln_data["id"]
-            if sln_id in seen_sln:
-                raise ValueError(f"Duplicate ID. Check if database solution IDs in question {q_id} are unique.")
-            seen_sln.add(sln_id)
-
-            _ = Solution(**sln_data)
-            _ = AiAnalysis(**sln_data["ai_analysis"])
-
-            client.collection(configs.question_collection) \
-                .document(q_id) \
-                .collection(configs.solution_collection) \
-                .document(sln_id) \
-                .set(sln_data)
-
-        _ = [TestCase(**test_data) for test_data in q_data["test_cases"]] # validate test cases
-
-        _ = Question(**q_data)  # validate question
-        client.collection(configs.question_collection).document(q_id).set(q_data)
+    utils.populate_database()
 
 
 def pytest_sessionfinish():
-    """ Clears the database when a test session ends. """
-    configs = Configs()
-    client = configs.firebase_manager.get_client()
-    questions = client.collection(configs.question_collection).stream()
-
-    for question in questions:
-        # delete solutions subcollection
-        solutions = client \
-            .collection(configs.question_collection) \
-            .document(question.id) \
-            .collection(configs.solution_collection) \
-            .stream()
-
-        for solution in solutions:
-            solution_ref = solution.reference
-            solution_ref.delete()
-
-        question_ref = question.reference
-        question_ref.delete()
+    utils.clear_database()
 
 
 @pytest.fixture()
--- tests/utils.py
+++ tests/utils.py
@@ -11,6 +11,65 @@ from app.schemas.test_case import TestCase
 from app.schemas.token import Token
 from .seed import QUESTIONS
 
+def populate_database():
+    """ Populates the database. """
+    configs = Configs()
+    client = configs.firebase_manager.get_client()
+
+    seen_q = set()
+    for q_data in QUESTIONS:
+        # sanity check for duplication
+        q_id = q_data["id"]
+        if q_id in seen_q:
+            raise ValueError("Duplicate ID. Check if database question IDs are unique.")
+        seen_q.add(q_id)
+
+        # validate solutions
+        seen_sln = set()
+        for sln_data in q_data["solutions"]:
+            # sanity check for duplication
+            sln_id = sln_data["id"]
+            if sln_id in seen_sln:
+                raise ValueError(f"Duplicate ID. Check if database solution IDs in question {q_id} are unique.")
+            seen_sln.add(sln_id)
+
+            _ = Solution(**sln_data)
+            _ = AiAnalysis(**sln_data["ai_analysis"])
+
+            client.collection(configs.question_collection) \
+                .document(q_id) \
+                .collection(configs.solution_collection) \
+                .document(sln_id) \
+                .set(sln_data)
+
+        _ = [TestCase(**test_data) for test_data in q_data["test_cases"]] # validate test cases
+
+        _ = Question(**q_data)  # validate question
+        client.collection(configs.question_collection).document(q_id).set(q_data)
+
+
+def clear_database():
+    """ Clears the database. """
+    configs = Configs()
+    client = configs.firebase_manager.get_client()
+    questions = client.collection(configs.question_collection).stream()
+
+    for question in questions:
+        # delete solutions subcollection
+        solutions = client \
+            .collection(configs.question_collection) \
+            .document(question.id) \
+            .collection(configs.solution_collection) \
+            .stream()
+
+        for solution in solutions:
+            solution_ref = solution.reference
+            solution_ref.delete()
+
+        question_ref = question.reference
+        question_ref.delete()
+
+
 def create_jwt_token(
     iss: str = ENV_VARS.get("JWT_ISS"),
     aud: str = ENV_VARS.get("JWT_AUD"),
@@ -26,6 +85,6 @@ def create_jwt_token(
 
     if enable_exp:
         payload.update({"exp": time.time() + (exp * 60)})
-        
+
     _ = Token(**payload)
     return jwt.encode(payload, jwt_secret, algorithm=hs_alg)
