cb2bd57fce187e2926be1b7024abf1727fad488c
ethanliu24 <liu.ethan2005@gmail.com>
2025-04-05 03:17:44 -0400

    scroll overflow contents

--- js/components/common/page.jsx
+++ js/components/common/page.jsx
@@ -5,8 +5,8 @@ export default function Page({ pageTitle, content, openSidebar }) {
 
   return (
     <div className="flex">
-    <Sidebar open={(openSidebar || JSON.parse(localStorage.getItem("openSidebar"))) && window.innerWidth >= 768} />
-      <div className="flex-1 py-4 px-16 max-md:px-4">
+      <Sidebar open={(openSidebar || JSON.parse(localStorage.getItem("openSidebar"))) && window.innerWidth >= 768} />
+      <div className="flex-1 h-screen overflow-y-scroll py-4 px-16 max-md:px-4">
         <h1 className="text-4xl font-medium mb-6">{pageTitle}</h1>
         <Content />
       </div>
