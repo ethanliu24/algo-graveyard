735d680725292a189edae15d421fb8510c4a0e42
ethanliu24 <liu.ethan2005@gmail.com>
2025-04-05 03:51:37 -0400

    add status icons

--- js/components/home/question_list.jsx
+++ js/components/home/question_list.jsx
@@ -1,4 +1,7 @@
 import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck, faHourglassEnd, faClock, faMinus } from "@fortawesome/free-solid-svg-icons";
+import { Tooltip } from "react-tooltip";
 import { getReqHeader } from "../../utils/api.js";
 
 export default function QuestionList() {
@@ -28,7 +31,7 @@ export default function QuestionList() {
         {questions.map((q, i) => {
           return (<ListItem key={"q" + i}
             idx={i + 1} title={q.title} status={q.status} source={q.source}
-            difficulty={q.difficulty} createdAt={q.createdAt} tags={q.tags}
+            difficulty={q.difficulty} createdAt={q.created_at} tags={q.tags}
           />)
         })
       }</div>
@@ -37,11 +40,28 @@ export default function QuestionList() {
 }
 
 function ListItem(props) {
+  const getStatusIcon = (status, idx) => {
+    const statusIcon = {
+      "completed": <FontAwesomeIcon icon={faCheck} color="#6bd177" />,
+      "unoptimized": <FontAwesomeIcon icon={faClock} color="#3b82f7" />,
+      "attempted": <FontAwesomeIcon icon={faHourglassEnd} color="#f5cd3d" />
+    };
+
+    const tooltip = statusIcon[status] ? status : "unknown";
+    const tooltipId = `${tooltip}-${idx}`;
+
+    return (
+      <div data-tooltip-id={tooltipId} data-tooltip-content={tooltip} data-tooltip-place="left">
+        {statusIcon[status] || <FontAwesomeIcon icon={faMinus} color="#c7c7c7" />}
+        <Tooltip id={tooltipId} />
+      </div>);
+  };
+
   return (
     <div className="flex justify-between items-center gap-4 text-center mt-4
       bg-transparent px-4 py-2 cursor-pointer rounded hover:bg-gray-200">
       <div className="text-[12px] text-gray-500">{props.idx}</div>
-      <div>{props.status}</div>
+      {getStatusIcon(props.status, props.idx)}
       <div className="flex-8 flex flex-col justify-between items-start">
         <div className="text-sm">{props.title}</div>
         <div className="flex justify-start items-center gap-2 text-[12px] text-gray-500">
