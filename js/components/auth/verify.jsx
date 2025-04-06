6b1a1e7623b3f3a5ddcc2f655a2d773d9a0806dc
ethanliu24 <liu.ethan2005@gmail.com>
2025-04-06 15:54:00 -0400

    added and styled toast

--- js/components/auth/verify.jsx
+++ js/components/auth/verify.jsx
@@ -1,18 +1,26 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { Password } from 'primereact/password';
+import { Toast } from 'primereact/toast';
 
 export default function Verify(props) {
   const [secret, setSecret] = useState("");
+  const verificationRes = useRef(null);
+
+  const handleVerification = () => {
+    let result = { severity: "info", summary: "Info", detail: "Content", sticky: true, className: "info" };
+    verificationRes.current.show(result);
+  };
 
   return (
     <div className={`flex flex-col justify-center items-center w-fit h-fit rounded-sm
       bg-gray-50 shadow-sm
-      ${props.classNames}`}>
-      <h2 className="w-full rounded-t-sm bg-primary text-white text-center">Authenticate</h2>
-      <div className="flex flex-col justify-around items-center gap-4 p-4">
+      ${props.className ? props.className : ""}`}>
+      <h2 className="w-full rounded-t-sm p-0.5 bg-primary text-white text-center">Authenticate</h2>
+      <div className="flex-1 flex flex-col justify-around items-center gap-4 p-4">
         <Password value={secret} onChange={(e) => setSecret(e.target.value)} feedback={false} tabIndex={1}
-          placeholder="Secret" className="p-0.5" />
-        <button className="w-full p-0.5 cursor-pointer">Verify</button>
+          placeholder="Secret" className="w-full" />
+        <Toast ref={verificationRes} position="bottom-right" />
+        <button className="w-full p-0.5 cursor-pointer" onClick={handleVerification}>Verify</button>
       </div>
     </div>
   )
