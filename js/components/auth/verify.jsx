8a4d60852580570c817edc59c0b7bed4f11ed800
ethanliu24 <liu.ethan2005@gmail.com>
2025-04-07 02:56:48 -0400

    style

--- js/components/auth/verify.jsx
+++ js/components/auth/verify.jsx
@@ -6,12 +6,12 @@ import { faXmark } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 /**
- * This component is closable, with the defualt of it being not.
- * To make it closable, set "props.closable" to true and provide a function at "props.closeComponent"
- * that unmounts the component.
+ * This component is closable. To make it closable, set "props.closable" to true and provide a
+ * function at "props.closeComponent" that unmounts the component.
  */
 export default function Verify(props) {
   const [secret, setSecret] = useState("");
+  const [show, setShow] = useState(true);
   const verificationRes = useRef(null);
 
   useEffect(() => {
@@ -21,10 +21,11 @@ export default function Verify(props) {
   }, []);
 
   const handleVerification = async () => {
-    let result = { severity: "danger", summary: "Error", detail: "Something went wrong.", className: "error" };
+    const life = 3000;
+    let result = { severity: "danger", summary: "Error", detail: "Something went wrong.", life: life, className: "error" };
 
     if (secret === "") {
-      result = { severity: "warning", summary: "Warning", detail: "Please enter app secret.", className: "warning" };
+      result = { severity: "warning", summary: "Warning", detail: "Please enter app secret.", life: life, className: "warning" };
     } else {
       const req = {
         method: "POST",
@@ -35,9 +36,14 @@ export default function Verify(props) {
       await fetch("api/auth", req)
         .then(response => {
           if (response.ok) {
-            result = { severity: "success", summary: "Success", detail: "You are authenticated!", className: "success" };
+            result = { severity: "success", summary: "Success", detail: "You are authenticated!", life: life, className: "success" };
+            setSecret("");
+            if (props.closable) {
+              setShow(false);
+              setTimeout(props.closeComponent(), life);
+            }
           } else {
-            result = { severity: "danger", summary: "Error", detail: "Authentication Failed.", className: "error" };
+            result = { severity: "danger", summary: "Error", detail: "Authentication Failed.", life: life, className: "error" };
           }
         })
         .catch(err => {
@@ -53,19 +59,20 @@ export default function Verify(props) {
       bg-gray-50 shadow-sm
       ${props.positionStyle ? props.positionStyle : "relative"}
       ${props.className ? props.className : ""}`}>
-      <h2 className="w-full rounded-t-sm p-0.5 bg-primary text-white text-center relative">Authenticate
-        {props.closable
-          ? <FontAwesomeIcon icon={faXmark} size="md" className="absolute top-1/2 right-0 -translate-y-1/2 cursor-pointer mr-2"
-              onClick={props.closeComponent} />
-          : null}
-      </h2>
-      <div className="flex-1 flex flex-col justify-around items-center gap-4 p-4">
-        <Password value={secret} onChange={(e) => setSecret(e.target.value)} feedback={false} tabIndex={1}
-          placeholder="Secret" className="w-full" />
-        <Toast ref={verificationRes} position="bottom-right" />
-        <button className="w-full p-0.5 cursor-pointer" onClick={handleVerification}>Verify</button>
+      <div className={`${show ? "" : "hidden"}`}>
+        <h2 className="w-full rounded-t-sm p-0.5 bg-primary text-white text-center relative">Authenticate
+          {props.closable
+            ? <FontAwesomeIcon icon={faXmark} size="md" className="absolute top-1/2 right-0 -translate-y-1/2 cursor-pointer mr-2"
+                onClick={props.closeComponent} />
+            : null}
+        </h2>
+        <div className="flex-1 flex flex-col justify-around items-center gap-4 p-4">
+          <Password value={secret} onChange={(e) => setSecret(e.target.value)} feedback={false} tabIndex={1}
+            placeholder="Secret" className="w-full" />
+          <button className="w-full p-0.5 cursor-pointer" onClick={handleVerification}>Verify</button>
+        </div>
       </div>
-
+      <Toast ref={verificationRes} position="bottom-right" />
     </div>
   )
 }
