9a5da6ccaaf9e7291cf49ea8f6e15fe1982efbce
ethanliu24 <liu.ethan2005@gmail.com>
2025-04-07 02:43:07 -0400

    make card closable

--- js/components/auth/authenticate.jsx
+++ js/components/auth/authenticate.jsx
@@ -3,7 +3,7 @@ import Verify from "../auth/verify.jsx";
 export default function Authenticate() {
   return (
     <div className="flex justify-center items-center w-full h-full">
-      <Verify />
+      <Verify closable={false} className="mt-[-15%]" />
     </div>
   );
 }
--- js/components/auth/verify.jsx
+++ js/components/auth/verify.jsx
@@ -2,7 +2,14 @@ import { useEffect, useState, useRef } from 'react';
 import { Password } from 'primereact/password';
 import { Toast } from 'primereact/toast';
 import { getReqHeader } from '../../utils/utils';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+/**
+ * This component is closable, with the defualt of it being not.
+ * To make it closable, set "props.closable" to true and provide a function at "props.closeComponent"
+ * that unmounts the component.
+ */
 export default function Verify(props) {
   const [secret, setSecret] = useState("");
   const verificationRes = useRef(null);
@@ -44,14 +51,21 @@ export default function Verify(props) {
   return (
     <div className={`flex flex-col justify-center items-center w-fit h-fit rounded-sm
       bg-gray-50 shadow-sm
+      ${props.positionStyle ? props.positionStyle : "relative"}
       ${props.className ? props.className : ""}`}>
-      <h2 className="w-full rounded-t-sm p-0.5 bg-primary text-white text-center">Authenticate</h2>
+      <h2 className="w-full rounded-t-sm p-0.5 bg-primary text-white text-center relative">Authenticate
+        {props.closable
+          ? <FontAwesomeIcon icon={faXmark} size="md" className="absolute top-1/2 right-0 -translate-y-1/2 cursor-pointer mr-2"
+              onClick={props.closeComponent} />
+          : null}
+      </h2>
       <div className="flex-1 flex flex-col justify-around items-center gap-4 p-4">
         <Password value={secret} onChange={(e) => setSecret(e.target.value)} feedback={false} tabIndex={1}
           placeholder="Secret" className="w-full" />
         <Toast ref={verificationRes} position="bottom-right" />
         <button className="w-full p-0.5 cursor-pointer" onClick={handleVerification}>Verify</button>
       </div>
+
     </div>
   )
 }
