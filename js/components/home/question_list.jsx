70b427b3ebfcfaec9443c9ddc920c9d858392819
ethanliu24 <liu.ethan2005@gmail.com>
2025-04-15 13:56:12 -0400

    go to q page when clicking on a q in homepage

--- js/components/home/question_list.jsx
+++ js/components/home/question_list.jsx
@@ -6,7 +6,7 @@ export default function QuestionList(props) {
     <div className="text-sm w-[100%] mb-12 flex flex-col justify-start items-center">
       {props.questions.map((q, i) => {
         return (<ListItem key={"q" + i}
-          idx={i + 1} title={q.title} status={q.status} source={q.source}
+          idx={i + 1} id={q.id} title={q.title} status={q.status} source={q.source}
           difficulty={q.difficulty} timestamp={q.last_modified} tags={q.tags}
         />);
       })}
@@ -17,7 +17,8 @@ export default function QuestionList(props) {
 function ListItem(props) {
   return (
     <div className="flex justify-between items-center gap-4 text-center w-[100%]
-      bg-transparent px-4 py-3 cursor-pointer rounded hover:bg-gray-200">
+      bg-transparent px-4 py-3 cursor-pointer rounded hover:bg-gray-200"
+      onClick={() => window.location.href = `/questions/${props.id}`}>
       <div className="text-[12px] text-gray-500">{props.idx}</div>
       {getStatusIcon(props.status, props.idx)}
       <div className="flex-8 flex flex-col justify-between items-start">
