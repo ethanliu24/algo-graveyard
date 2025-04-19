2e1c50acacc529f47cd5281ed7f127d71c55e983
ethanliu24 <liu.ethan2005@gmail.com>
2025-04-18 23:55:51 -0400

    display sln

--- js/components/question/solutions_tab.jsx
+++ js/components/question/solutions_tab.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
+import { faCheck, faX } from "@fortawesome/free-solid-svg-icons";
 import ModalContainer from "../common/modal";
 import SolutionForm from "./solution_form";
+import { formatDate } from "../../utils/utils";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function SolutionTab(props) {
   const [solutions, setSolutions] = useState(props.solutions || []);
@@ -9,12 +12,24 @@ export default function SolutionTab(props) {
   const creationSucess = (slnData) => {
     setOpenForm(false);
     setSolutions([...solutions, slnData]);
-    console.log(slnData)
   }
 
   return (
     <div className="flex flex-col justify-start items-start w-full">
-      <button onClick={() => setOpenForm(true)}>Add solution</button>
+      <button className="mb-2" onClick={() => setOpenForm(true)}>Add solution</button>
+      {solutions.map((sln, i) => {
+        return (
+          <div className="flex justify-start items-center gap-4 w-full hover:bg-gray-200 cursor-pointer
+            px-4 py-2 text-[12px] rounded">
+            <div className="text-gray-500">{i + 1}</div>
+            <FontAwesomeIcon icon={sln.accepted ? faCheck : faX} color={sln.accepted ? "#6bd177" : "#eb4b63"} />
+            <div className="flex-1">
+              <h1 className="text-base">{sln.summary}</h1>
+              <h2 className="text-nowrap truncate">{formatDate(sln.last_modified)}</h2>
+            </div>
+          </div>
+        );
+      })}
       {openForm
         ? <ModalContainer closeModal={() => setOpenForm(false)} title="Add Solution"
             content={<SolutionForm create={true} questionId={props.questionId} methodSuccessful={(d) => creationSucess(d)} />} />
