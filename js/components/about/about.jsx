e9943d656178e23671d294c3e9459d8fdbce0faa
ethanliu24 <liu.ethan2005@gmail.com>
2025-04-25 23:00:34 -0400

    modify replace content

--- js/components/about/about.jsx
+++ js/components/about/about.jsx
@@ -2,6 +2,6 @@ import ReactMarkdown from 'react-markdown';
 import about from "../../../README.md?raw";
 
 export default function About() {
-    const aboutContent = about.replace("Algo Graveyard", "").trim();
+    const aboutContent = about.replace("# Algo Graveyard", "").trim();
     return <div className="prose prose-sm"><ReactMarkdown children={aboutContent} /></div>;
 }
