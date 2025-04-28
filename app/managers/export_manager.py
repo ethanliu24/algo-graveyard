303d784cdb5ff29eedd64685d09fa1fbd1eacb6a
ethanliu24 <liu.ethan2005@gmail.com>
2025-04-28 18:40:00 -0400

    add basic styling to pdf export

--- app/managers/export_manager.py
+++ app/managers/export_manager.py
@@ -1,4 +1,5 @@
 import markdown
+from weasyprint import HTML, CSS
 from ..managers.question_manager import QuestionManager
 from ..schemas.question import Question
 from ..schemas.solution import Solution
@@ -14,28 +15,39 @@ class ExportManager:
 
         html = ""
         html += markdown.markdown(self._format_question_md(question))
+        html += "# Solutions"
 
-        solution_htmls = []
         for sln in question.solutions:
             if sln.id in solution_ids:
                 sln_html = markdown.markdown(self._format_solution_md(sln))
-                solution_htmls.append(sln_html)
+                html += sln_html
+
+        html = html.replace("```", "")
+        HTML(string=html).write_pdf("z.pdf", stylesheets=[CSS("app/templates/export.css")])
+        return html
 
     def _format_question_md(self, question: Question) -> str:
         md = ""
         md += f"# {question.title} - {question.status.value.capitalize()}\n"
-        md += f"Difficulty: {question.difficulty.value.capitalize()}\n"
-        md += f"{question.source.value.capitalize()}: [{question.link}]({question.link})\n"
-        md += f"Created at: {question.created_at.strftime("%Y-%m-%d")}\n"
-        md += f"Created at: {question.created_at.strftime("%Y-%m-%d")}\n"
-        md += f"\n"
+        md += f"Difficulty: {question.difficulty.value.capitalize()}\n\n"
+        md += f"{question.source.value.capitalize()}: [{question.link}]({question.link})\n\n"
+        md += f"Created at: {question.created_at.strftime("%Y-%m-%d")}\n\n"
+        md += f"Created at: {question.created_at.strftime("%Y-%m-%d")}\n\n"
+        md += f"### Description:\n"
         md += f"{question.prompt}\n\n"
-        md += f"### Notes\n"
-        md += f"{"\n".join([f"- {note}" for note in question.notes])}\n\n"
-        md += f"### Hints\n"
-        md += f"{"\n".join([f"- {hint}" for hint in question.hints])}\n\n"
-        md += f"### Tags\n"
-        md += f"{", ".join([tag.value for tag in question.tags])}\n\n"
+
+        if question.notes:
+            md += f"### Notes\n"
+            md += f"{"\n".join([f"- {note}" for note in question.notes])}\n\n"
+
+        if question.hints:
+            md += f"### Hints\n"
+            md += f"{"\n".join([f"- {hint}" for hint in question.hints])}\n\n"
+
+        if question.tags:
+            md += f"### Tags\n"
+            md += f"{", ".join([tag.value for tag in question.tags])}\n\n"
+
         return md
 
     def _format_solution_md(self, solution: Solution) -> str:
@@ -49,7 +61,7 @@ class ExportManager:
         md += f"- Time: O({solution.time_complexity})\n- Space: O({solution.space_complexity})\n"
         md += f"{solution.explanation}\n\n"
         md += f"### AI Analysis\n\n"
-        md += f"- Time: O({solution.ai_analysis.time_complexity})\n- Space: O({solution.ai_analysis.space_complexity})\n"
-        md += f"{solution.ai_analysis.feedback}"
-        md += f"```\n{solution.code}\n```\n\n"
+        md += f"- Time: O({solution.ai_analysis.time_complexity})\n- Space: O({solution.ai_analysis.space_complexity})\n\n"
+        md += f"{solution.ai_analysis.feedback}\n\n"
+        md += f"```{solution.code}```\n\n"
         return md
